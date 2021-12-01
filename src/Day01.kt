c829c06f2d581a2a2e32c4dbcb985757388d2a26
Artyom Dorosh <artyom.dorosh@cuvva.com>
2021-12-01 11:51:16 +0000

    2nd part of Day 1 task

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,31 @@
 fun main() {
     val testInput = readInput("Day01_test")
-    check(solve(testInput) == 8)
+    check(solveTask1(testInput) == 8)
+    check(solveTask2(testInput) == 6)
 
     val input = readInput("Day01")
-    println(solve(input))
+    println(solveTask1(input))
+    println(solveTask2(input))
 }
 
-fun solve(input: List<String>): Int =
-        input
-                .map { value -> value.toInt() }
-                .fold(Result()) { acc, value ->
-                    val newCounter = when {
-                        acc.previousHighest != null && value > acc.previousHighest -> acc.counter + 1
-                        else -> acc.counter
-                    }
-                    acc.copy(previousHighest = value, counter = newCounter)
-                }.counter
+fun solveTask1(input: List<String>): Int =
+        input.map { value -> value.toInt() }
+                .solveIncreasingTimes()
+
+fun solveTask2(input: List<String>): Int = input.map { value -> value.toInt() }
+        .windowed(size = 3, step = 1, partialWindows = false) { list ->
+            list.sum()
+        }
+        .solveIncreasingTimes()
+
+
+internal fun Iterable<Int>.solveIncreasingTimes(): Int = fold(Result()) { acc, value ->
+    val newCounter = when {
+        acc.previousHighest != null && value > acc.previousHighest -> acc.counter + 1
+        else -> acc.counter
+    }
+    acc.copy(previousHighest = value, counter = newCounter)
+}.counter
 
 data class Result(
         val previousHighest: Int? = null,
