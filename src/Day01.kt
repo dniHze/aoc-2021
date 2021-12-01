0827997ab6c4b0e20635b37276d1fc977aefd378
Artyom Dorosh <artyom.dorosh@cuvva.com>
2021-12-01 11:33:57 +0000

    First try to solve Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,23 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(solve(testInput) == 8)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println(solve(input))
 }
+
+fun solve(input: List<String>): Int =
+        input
+                .map { value -> value.toInt() }
+                .fold(Result()) { acc, value ->
+                    val newCounter = when {
+                        acc.previousHighest != null && value > acc.previousHighest -> acc.counter + 1
+                        else -> acc.counter
+                    }
+                    acc.copy(previousHighest = value, counter = newCounter)
+                }.counter
+
+data class Result(
+        val previousHighest: Int? = null,
+        val counter: Int = 0,
+)
