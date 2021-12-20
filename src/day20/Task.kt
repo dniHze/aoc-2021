3d853e594d632eb3bc1d4bda270dacff7c29ad3f
GitHub Action <action@github.com>
2021-12-20 06:07:29 +0000

    Generate today files from template

---
+++ src/day20/Task.kt
@@ -0,0 +1,13 @@
+package day20
+
+import readInput
+
+fun main() {
+    val input = readInput("day20")
+    println(solvePartOne(input))
+    println(solvePartTwo(input))
+}
+
+fun solvePartOne(input: List<String>): Int = 0
+
+fun solvePartTwo(input: List<String>): Int = 0
